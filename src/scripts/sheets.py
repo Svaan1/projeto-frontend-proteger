289e3caaa59344a3b0cb37eedf0321e06d1ce6f1
Paulo Ricardo <plricardo504@gmail.com>
2024-03-19 02:20:50 -0300

    fix: add missing age and sex data fields

--- src/scripts/sheets.py
+++ src/scripts/sheets.py
@@ -2,8 +2,8 @@ import pandas
 
 from datetime import datetime
 
-TWO_DIGIT_DATE_FORMAT = '%d/%m/%y'
-FOUR_DIGIT_DATE_FORMAT = '%d/%m/%Y'
+TWO_DIGIT_YEAR_DATE_FORMAT = '%d/%m/%y'
+FOUR_DIGIT_YEAR_DATE_FORMAT = '%d/%m/%Y'
 
 RESIDENT_QUESTION_RESULTS = {
     # Restrição de Mobilidade - range(3, 9)
@@ -13,15 +13,19 @@ RESIDENT_QUESTION_RESULTS = {
     6: 'ANDA SEM SUPORTE MAS COM DIFICULDADE',
     7: 'ANDA SEM RESTRIÇÕES',
     8: 'SEM INFORMAÇÃO',
+    # Deficiência Sensorial - range()
     9: 'DEFICIENTE AUDITIVO',
     10: 'DEFICIENTE VISUAL',
     11: 'SEM INFORMAÇÃO',
+    # Portador de Transtorno/Deficiência Mental
     12: 'SIM',
     13: 'NÃO',
     14: 'SEM INFORMAÇÃO',
+    # Algum outro agravo de saúde que atrapalhe a evacuação em situação de emergência
     15: 'SIM',
     16: 'NÃO',
     17: 'SEM INFORMAÇÃO',
+    # Educação
     18: 'NENHUMA',
     19: 'ENSINO FUNDAMENTAL',
     20: 'ENSINO MÉDIO',
@@ -29,6 +33,7 @@ RESIDENT_QUESTION_RESULTS = {
     22: 'SEM INFORMAÇÃO',
 }
 
+# Main function
 def transform_file_data(file):
     dataframe = pandas.read_excel(file, sheet_name='dados')
 
@@ -37,12 +42,12 @@ def transform_file_data(file):
 
     for index, row in dataframe.iterrows():
 
-        forms.append(get_current_form(row))
+        forms.append(get_form(row))
         residents.append(get_residents(row))
 
     return forms, residents
-            
-def get_current_form(row):
+
+def get_form(row):
     current_form = {}
 
     date = row['data']
@@ -52,9 +57,9 @@ def get_current_form(row):
             current_form['date'] = date.date()
         if type(date) is str:
             try:
-                current_form['date'] = datetime.strptime(date, TWO_DIGIT_DATE_FORMAT).date()
+                current_form['date'] = datetime.strptime(date, TWO_DIGIT_YEAR_DATE_FORMAT).date()
             except:
-                current_form['date'] = datetime.strptime(date, FOUR_DIGIT_DATE_FORMAT).date()
+                current_form['date'] = datetime.strptime(date, FOUR_DIGIT_YEAR_DATE_FORMAT).date()
     
     district = row['bairro pela data']
     if is_valid(district, str):
@@ -89,7 +94,7 @@ def get_current_form(row):
 def get_residents(row):
     residents = []
     
-    # Get Main Resident
+    # Main resident has to have separate logic because of the different ways the same information is stored
     main_resident = {}
 
     sex = row['sexo']
@@ -106,34 +111,42 @@ def get_residents(row):
         
     residents.append(main_resident)
 
-    # Get secondary residents
     number_of_residents = row['#pessoas']
-
     if not is_valid(number_of_residents, int):
         return residents
     
-    for resident in range(number_of_residents):
+    for resident_index in range(number_of_residents):
         current_resident = {}
+                
+        sex = row['outros mor 1'].split('/')[resident_index]
+        if sex:
+            current_resident['sex'] = sex[0].upper()
 
-        current_resident['mobility_restriction'] = find_marked_option(row, current_resident, resident, range(3, 9))
-        current_resident['sensory_impairment'] = find_marked_option(row, current_resident, resident, range(9, 12))
-        current_resident['mental_disorder'] = find_marked_option(row, current_resident, resident, range(12, 15))
-        current_resident['health_injury'] = find_marked_option(row, current_resident, resident, range(15, 18))
-        current_resident['education'] = find_marked_option(row, current_resident, resident, range(18, 23))
+        age = row['outros mor 2'].split('/')[resident_index]
+        if age and age.isdigit():
+            current_resident['age'] = int(age)
+
+        current_resident['mobility_restriction'] = find_marked_option(row, resident_index, range(3, 9))
+        current_resident['sensory_impairment'] = find_marked_option(row, resident_index, range(9, 12))
+        current_resident['mental_disorder'] = find_marked_option(row, resident_index, range(12, 15))
+        current_resident['health_injury'] = find_marked_option(row, resident_index, range(15, 18))
+        current_resident['education'] = find_marked_option(row, resident_index, range(18, 23))
 
         residents.append(current_resident)
 
     return residents
 
-def find_marked_option(row, current_resident, resident, option_range):
+# Finds where the X is marked and gets the answer that it points to
+def find_marked_option(row, resident_index, option_range):
     for option_number in option_range:
         current_option = row[f'outros mor {option_number}']
 
-        result = current_option.split("/")[resident]
+        result = current_option.split("/")[resident_index]
 
         if result.lower() == 'x':
             return RESIDENT_QUESTION_RESULTS[option_number]
 
+# Defines whats a valid value for the model object
 def is_valid(value, instance=None):
     if pandas.isnull(value):
         return False
@@ -143,7 +156,7 @@ def is_valid(value, instance=None):
     
     if type(value) is str and value.startswith('#'):
         return False
-    
+        
     return True
 
 
