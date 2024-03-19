b4a9cf46abc7ab3c8d9f419639756deea2f91c9d
Paulo Ricardo <plricardo504@gmail.com>
2024-03-19 05:24:34 -0300

    feat: add storage saving and deleting for files

--- src/routes/files.py
+++ src/routes/files.py
@@ -10,6 +10,9 @@ from src.models.file import UploadResponse
 
 from sqlalchemy.exc import IntegrityError
 
+import shutil
+from pathlib import Path
+
 router = APIRouter(prefix="/files")
 
 @router.get("/", response_model=list[UploadResponse])
@@ -40,16 +43,29 @@ def upload_file(file: UploadFile | None = None, active_user=Depends(manager), db
 
     create_forms_and_residents_from_list(upload, forms, residents, db)
 
+    with open(f"./src/static/uploads/{file.filename}", "wb") as f:
+            shutil.copyfileobj(file.file, f)
+
     db.commit()
+    return
     
 @router.delete("/{upload_id}", status_code=204)
 def delete_file(upload_id: int, active_user=Depends(manager), db=Depends(get_session)) -> None:
     upload = get_upload_by_id(upload_id, db)
+
+    if not upload:
+        return
 
     if upload.user_id != active_user.id:
         raise InvalidPermissions
     
     delete_upload_if_exists(upload_id, db)
 
+    file_path = Path("src/static/uploads/" + upload.filename)
+
+    if file_path.exists():
+        file_path.unlink()
+
+    db.commit()
     return
 
