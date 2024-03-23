52103fa8b3a26583f21c27de07ec139662953469
Paulo Ricardo <plricardo504@gmail.com>
2024-03-22 21:16:00 -0300

    refactor: fix html and css imports

--- src/main.py
+++ src/main.py
@@ -1,41 +1,37 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 from src.routes.auth import router as auth_router
 from src.routes.user import router as user_router
 from src.db import create_tables
 
-create_tables()  # Create the tables in the database if they don't exist
+create_tables()
 app = FastAPI()
 
-templates = Jinja2Templates(directory="templates")
+templates = Jinja2Templates(directory="src/templates")
+app.mount("/static", StaticFiles(directory="src/static"), name="static")
 
 @app.get("/", response_class=HTMLResponse)
 async def root(request: Request):
-    # Redirect to the home page
     return RedirectResponse(url="/home")
 
 @app.get("/home", response_class=HTMLResponse)
 async def home(request: Request):
-    # Render the home.html template
     return templates.TemplateResponse("home.html", {"request": request})
 
-@app.get("/index", response_class=HTMLResponse)
-async def index(request: Request):
-    # Redirect to the home page
-    return RedirectResponse(url="/home")
+@app.get("/login", response_class=HTMLResponse)
+async def login(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
 
-@app.get("/upload.html", response_class=HTMLResponse)
+@app.get("/register", response_class=HTMLResponse)
+async def register(request: Request):
+    return templates.TemplateResponse("register.html", {"request": request})
+
+@app.get("/upload", response_class=HTMLResponse)
 async def upload(request: Request):
-    # Render the upload.html template
     return templates.TemplateResponse("upload.html", {"request": request})
 
 app.include_router(auth_router)
 app.include_router(user_router)
-
-# @app.get ("/items/{id}", response_class=HTMLResponse)
-# async def read_item(request: Request, id: str):
-#   return templates.TemplateResponse(
-#       request=request, name="item.html", context={"id": id}
-#   )
