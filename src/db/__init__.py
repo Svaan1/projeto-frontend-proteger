cef03f2f7ccd1e0ecd052d1c5e10393be3b69574
Paulo Ricardo <plricardo504@gmail.com>
2024-04-03 06:14:30 -0300

    feat: organize templates and link interface to api

--- src/db/__init__.py
+++ src/db/__init__.py
@@ -1,9 +1,16 @@
-from sqlalchemy import create_engine
+from sqlalchemy import URL, create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 from os import getenv
 
 Base = declarative_base()
-engine = create_engine(getenv("DATABASE_URL"))
+url_object = URL.create(
+    drivername='postgresql+psycopg2',
+    host=getenv('DATABASE_HOST'),
+    username=getenv('DATABASE_USER'),
+    password=getenv('DATABASE_PASSWORD'),
+    database=getenv('DATABASE_NAME')
+)
+engine = create_engine(url_object)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
--- src/routes/user.py
+++ src/routes/user.py
@@ -9,19 +9,6 @@ from fastapi import APIRouter, Depends
 router = APIRouter(prefix="/user")
 
 
-@router.get("/{username}")
-def read_user(
-    username, active_user=Depends(manager), db=Depends(get_session)
-) -> UserResponse:
-    """
-    Shows information about the user
-    """
-    user = get_user_by_name(username, db)
-
-    if user is None:
-        raise InvalidUserName
-
-    if user.username != active_user.username:
-        raise InvalidPermissions
-
-    return UserResponse.model_validate(user)
+@router.get("/")
+def read_user(active_user=Depends(manager)) -> UserResponse:
+    return UserResponse.model_validate(active_user)
