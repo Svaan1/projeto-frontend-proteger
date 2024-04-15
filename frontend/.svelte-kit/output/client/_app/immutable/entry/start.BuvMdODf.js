6e3c90df409cf86cf6e7a54b702d6099ad68d5b6
Paulo Ricardo <plricardo504@gmail.com>
2024-04-15 08:50:41 -0300

    feat: add frontend container configuration

---
+++ frontend/.svelte-kit/output/client/_app/immutable/entry/start.BuvMdODf.js
@@ -0,0 +1,1 @@
+import{b as a}from"../chunks/entry.BYnaP5r2.js";export{a as start};
---
+++ frontend/.svelte-kit/output/server/nodes/1.js
@@ -0,0 +1,8 @@
+
+
+export const index = 1;
+let component_cache;
+export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
+export const imports = ["_app/immutable/nodes/1.CGAco6zv.js","_app/immutable/chunks/scheduler.Dk-snqIU.js","_app/immutable/chunks/index.hN5g6nR9.js","_app/immutable/chunks/stores.9n-o8nJr.js","_app/immutable/chunks/entry.BYnaP5r2.js","_app/immutable/chunks/index.Ice1EKvx.js"];
+export const stylesheets = [];
+export const fonts = [];
