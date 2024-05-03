b79a22a55d6801f3f93f855db36cb5aaeaa4c659
Paulo Ricardo <plricardo504@gmail.com>
2024-05-03 01:21:09 -0300

    feat: add download file functionality

--- frontend/src/lib/components/DashboardFileTable.svelte
+++ frontend/src/lib/components/DashboardFileTable.svelte
@@ -1,6 +1,13 @@
 <script>
     import * as Table from "$lib/components/ui/table";
 
+    import { Download, Trash2 } from 'lucide-svelte'
+    import { goto } from "$app/navigation";
+
+    async function handleGetFile(fileId) {
+        await goto("/dashboard/files/" + fileId);
+    }
+
     export let data;
 </script>
 
@@ -9,14 +16,30 @@
     <Table.Header>
       <Table.Row>
         <Table.Head>Nome do Arquivo</Table.Head>
-        <Table.Head>Data</Table.Head>
+            <Table.Head>Data</Table.Head>
       </Table.Row>
     </Table.Header>
     <Table.Body>
       {#each data as data (data.id)}
         <Table.Row>
-          <Table.Cell>{data.filename}</Table.Cell>
-          <Table.Cell>{data.datetime}</Table.Cell> <!-- new Intl.DateTimeFormat('pt-BR').format(data.datetime) -- formatar a data!!! -->
+            <Table.Cell>{data.filename}</Table.Cell>
+            <Table.Cell>{data.datetime}</Table.Cell>
+            <Table.Cell>
+                <button
+                        on:click={() => {handleGetFile(data.id)}}
+                        class="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+                >
+                    <Download size="18"/>
+                </button>
+            </Table.Cell>
+            <Table.Cell>
+                <button
+                        on:click={() => {}}
+                        class="flex h-9 w-9 items-center justify-center rounded-lg text-red-500 transition-colors hover:text-red-700 md:h-8 md:w-8"
+                >
+                    <Trash2 size="18"/>
+                </button>
+            </Table.Cell>
         </Table.Row>
       {/each}
     </Table.Body>
