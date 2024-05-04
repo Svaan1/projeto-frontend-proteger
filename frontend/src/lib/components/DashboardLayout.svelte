f5246630c6e82d23dcb5ea9dd27b4a83806108a5
Matheus Motizuki <123515571+MatheusMotizuki@users.noreply.github.com>
2024-05-04 00:44:47 +0000

    Revert "fix: typos"
    This reverts commit af066c201a1bba97622f4315cf7b3450596aabd4.

--- frontend/src/lib/components/DashboardLayout.svelte
+++ frontend/src/lib/components/DashboardLayout.svelte
@@ -37,7 +37,7 @@
             <Tooltip.Root>
                 <Tooltip.Trigger asChild let:builder>
                     <button
-                            on:click={() => currentView.set("graphs")}
+                            on:click={() => setView("graphs")}
                             class:active={$currentView === 'graphs'}
                             class="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
                             use:builder.action
@@ -52,7 +52,7 @@
             <Tooltip.Root>
                 <Tooltip.Trigger asChild let:builder>
                     <button
-                            on:click={() => currentView.set("files")}
+                            on:click={() => setView("files")}
                             class:active={$currentView === 'files'}
                             class="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
                             use:builder.action
@@ -67,7 +67,7 @@
             <Tooltip.Root>
                 <Tooltip.Trigger asChild let:builder>
                     <button
-                            on:click={() => currentView.set("users")}
+                            on:click={() => setView("users")}
                             class:active={$currentView === 'users'}
                             class="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
                             use:builder.action
@@ -84,27 +84,7 @@
             <Tooltip.Root>
                 <Tooltip.Trigger asChild let:builder>
                     <button
-                        class="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
-                        use:builder.action
-                        {...builder}
-                    >
-                    <div class="icon-toggle">
-                        <button class="icons" on:click={toggleIcon}>
-                            {#if isSunVisible}
-                                <Sun class="h-5 w-5" />
-                            {:else}
-                                <Moon class="h-5 w-5" />
-                            {/if}
-                        </button>
-                    </div>
-                        <span class="sr-only">togglelights</span>
-                    </button>
-                </Tooltip.Trigger>
-                <Tooltip.Content side="right">Sair</Tooltip.Content>
-            </Tooltip.Root>
-            <Tooltip.Root>
-                <Tooltip.Trigger asChild let:builder>
-                    <button
+                        on:click={() => currentView.set("settings")}
                         class="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
                         use:builder.action
                         {...builder}
