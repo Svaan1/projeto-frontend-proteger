91139f841cf4c561a44b62e0c12bbb4f9320646b
Matheus Motizuki <123515571+MatheusMotizuki@users.noreply.github.com>
2024-05-29 19:28:48 -0300

    Revert "feat: add homepage and toasts"
    This reverts commit 02893a9cbb972f0296746ee56c7ea2dbef91faa7.

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,6 +1,1 @@
-<script>
-    import "../app.pcss";
-    import { Toaster } from "$lib/components/ui/sonner";
-</script>
-<Toaster richColors expand={true} />
-<slot></slot>
+<script>import "../app.pcss";</script><slot></slot>
