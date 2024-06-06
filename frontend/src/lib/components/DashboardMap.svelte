39954f7d5154b75ff4e6ca97dac3cc2b88357257
Gustavo B. <gustavobrunodemelo@gmail.com>
2024-06-05 21:22:22 -0300

    feat: put map dashboard in its own a tab

--- frontend/src/lib/components/DashboardMap.svelte
+++ frontend/src/lib/components/DashboardMap.svelte
@@ -2,6 +2,7 @@
   import { Search } from "lucide-svelte";
   import MultiSelectComboBoxGrafana from "./MultiSelectComboBoxGrafana.svelte";
   import Button from "./ui/button/button.svelte";
+  import * as Tabs from "$lib/components/ui/tabs";
 
   let selectedQuestions = [];
   const baseURL = "http://localhost:3000/d-solo/adhqdflvh15ogd/map-dashboard?orgId=1&panelId=1"; // eventually change this to the actual URL inside docker
@@ -19,12 +20,26 @@
   }
 </script>
 
-<div class="flex items-center display-grid gap-4"> 
-    <MultiSelectComboBoxGrafana  bind:values={selectedQuestions}/>
-    <Button on:click={handleClick}>
-        <Search class="h-5 w-5"/>
-    </Button>
-</div>
+<Tabs.Root value="map" class="w-[400px]">
+  <Tabs.List>
+    <Tabs.Trigger value="map">Mapa</Tabs.Trigger>
+    <Tabs.Trigger value="main">Outros Dashboards</Tabs.Trigger>
+  </Tabs.List>
 
-<iframe title="map-dashboard" src={dashboardURL} width="85%" height="70%" frameborder="0"/>
+  <Tabs.Content value="map">
+    <div class="flex items-center display-grid gap-4"> 
+      <MultiSelectComboBoxGrafana  bind:values={selectedQuestions}/>
+      <Button on:click={handleClick}>
+          <Search class="h-5 w-5"/>
+      </Button>
+    </div>
+    
+    <iframe title="map-dashboard" src={dashboardURL} width="85%" height="70%" frameborder="0"/>
+  </Tabs.Content>
+
+  <Tabs.Content value="main">
+      other dashboards will be here in the future.
+  </Tabs.Content>
+</Tabs.Root>
+
 
