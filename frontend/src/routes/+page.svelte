9c286990ea74bc9d9c96aa9327b8e232344f3a8d
Matheus Motizuki <123515571+MatheusMotizuki@users.noreply.github.com>
2024-06-07 00:58:41 +0000

    feat: add landing page

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,16 +1,44 @@
-<link rel="preconnect" href="https://fonts.googleapis.com">
+<!-- <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Rokkitt:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Rokkitt:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"> -->
 
-<body class="flex items-center justify-center min-h-screen bg-sky-600">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Smooth Scroll Example</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet">
 
-    <div class="background"></div>
-    <div class="overlay"></div>
-    <h1 class="absolute inset-0 flex items-center justify-center select-none text-9xl opacity-50 z-10 text-yellow-300">Proteger</h1>
-    <button on:click={() => window.location.href = '/login'} id="btnSubmit" type="submit" class="relative z-20 px-6 py-2 mr-4 text-white bg-black rounded-md shadow-lg transition-all duration-300 transform hover:outline-2 hover:scale-105 focus:outline-2 focus:ring-4 ">
-        Login
-    </button>
-    <button on:click={() => window.location.href = '/register'} id="btnSubmit" type="submit" class="relative z-20 px-6 py-2 text-white bg-transparent outline outline-black outline-2 rounded-md shadow-lg transition-all duration-300 transform hover:outline-2 hover:scale-105 focus:outline-2 focus:ring-4 ">
-        Registrar
-    </button>
+</head>
+
+
+<body class="flex items-center justify-center min-h-screen bg-teal-700">
+    <main>
+        <ol>
+            <li>
+                <button on:click={() => window.location.href = 'https://www.unifeso.edu.br/'} class="absolute top-5 inset-x-0 z-20 px-4 py-2 text-black text-4xl">Unifeso</button>
+            </li>
+            <li>
+                <div>
+                    <div class="flex justify-center items-center">
+                        <h2 class="font-thin select-none z-99 text-4xl text-white subpixel-antialiased tracking-widest">T E R E S O P Ó L I S</h2>
+                    </div>
+                    <div>
+                        <h1 class="font-mono z-99 select-none text-9xl text-white subpixel-antialiased animate-bounce text-center md:min-w-16">Projeto Proteger</h1>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="flex justify-center items-center">
+                    <button on:click={() => window.location.href = '/login'} id="btnTop" type="submit" class="mx-2 px-4 py-2 text-white outline outline-black outline-1 bg-transparent rounded-md shadow-lg transition-all duration-300 transform hover:scale-105 focus:outline-2 focus:ring-4">
+                        Logar
+                    </button>
+                    <button on:click={() => window.location.href = '/register'} id="btnTop" type="submit" class="mx-2 px-4 py-2 text-white bg-black rounded-lg shadow-lg transition-all duration-300 transform hover:scale-105 focus:outline-2 focus:ring-4">
+                        Registrar
+                    </button>
+                </div>
+            </li>            
+        </ol>
+    </main>
 </body>
