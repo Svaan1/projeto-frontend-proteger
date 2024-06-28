9ad7d62f648c8b73352c8a19843c45fe51951d8d
Matheus Motizuki <123515571+MatheusMotizuki@users.noreply.github.com>
2024-06-28 22:51:18 +0000

    chore: change color

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -9,11 +9,9 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet">
-
 </head>
 
-
-<body class="flex items-center justify-center min-h-screen bg-teal-700">
+<body class="flex items-center justify-center min-h-screen bg-white">
     <main>
         <ol>
             <li>
@@ -22,16 +20,16 @@
             <li>
                 <div>
                     <div class="flex justify-center items-center">
-                        <h2 class="font-thin select-none z-99 text-4xl text-white subpixel-antialiased tracking-widest">T E R E S O P Ó L I S</h2>
+                        <h2 class="font-thin select-none z-99 text-4xl text-black subpixel-antialiased tracking-widest">T E R E S O P Ó L I S</h2>
                     </div>
                     <div>
-                        <h1 class="font-mono z-99 select-none text-9xl text-white subpixel-antialiased animate-bounce text-center md:min-w-16">Projeto Proteger</h1>
+                        <h1 class="font-mono z-99 select-none text-9xl text-black subpixel-antialiased animate-bounce text-center md:min-w-16">Projeto Proteger</h1>
                     </div>
                 </div>
             </li>
             <li>
                 <div class="flex justify-center items-center">
-                    <button on:click={() => window.location.href = '/login'} id="btnTop" type="submit" class="mx-2 px-4 py-2 text-white outline outline-black outline-1 bg-transparent rounded-md shadow-lg transition-all duration-300 transform hover:scale-105 focus:outline-2 focus:ring-4">
+                    <button on:click={() => window.location.href = '/login'} id="btnTop" type="submit" class="mx-2 px-4 py-2 text-black outline outline-black outline-1 bg-transparent rounded-md shadow-lg transition-all duration-300 transform hover:scale-105 focus:outline-2 focus:ring-4">
                         Logar
                     </button>
                     <button on:click={() => window.location.href = '/register'} id="btnTop" type="submit" class="mx-2 px-4 py-2 text-white bg-black rounded-lg shadow-lg transition-all duration-300 transform hover:scale-105 focus:outline-2 focus:ring-4">
