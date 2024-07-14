f3272556ac69b5b878ccf88344ad510b2186cc1d
Gustavo B. <gustavobrunodemelo@gmail.com>
2024-07-14 10:59:22 -0300

    refactor: hacky way to bypass incomplete xlsx

--- backend/src/scripts/sheets.py
+++ backend/src/scripts/sheets.py
@@ -35,6 +35,8 @@ RESIDENT_QUESTION_RESULTS = {
 }
 
 # Main function
+
+
 def transform_file_data(file):
     dataframe = pandas.read_excel(file, sheet_name='dados')
 
@@ -48,20 +50,23 @@ def transform_file_data(file):
 
     return forms, residents
 
+
 def get_form(row):
     current_form = {}
 
     date = row['data']
     if is_valid(date):
-        
+
         if type(date) is datetime:
             current_form['date'] = date.date()
         if type(date) is str:
             try:
-                current_form['date'] = datetime.strptime(date, TWO_DIGIT_YEAR_DATE_FORMAT).date()
+                current_form['date'] = datetime.strptime(
+                    date, TWO_DIGIT_YEAR_DATE_FORMAT).date()
             except:
-                current_form['date'] = datetime.strptime(date, FOUR_DIGIT_YEAR_DATE_FORMAT).date()
-    
+                current_form['date'] = datetime.strptime(
+                    date, FOUR_DIGIT_YEAR_DATE_FORMAT).date()
+
     district = row['bairro pela data']
     if is_valid(district, str):
         current_form['district'] = district
@@ -69,7 +74,7 @@ def get_form(row):
     address = row['end']
     if is_valid(address, str):
         current_form['address'] = address.title()
-    
+
     coord_s = row['coord S2']
     if is_valid(coord_s, float):
         current_form['coord_s'] = coord_s
@@ -89,38 +94,41 @@ def get_form(row):
     ivee = row['IVEE']
     if is_valid(ivee, int):
         current_form['ivee'] = ivee
-    
+
     # Get questions
     field_pattern = r'^q\d{1,2}(?:[-\s]\d+)?(?: [A-C])?$'
 
     field_list = row.index.to_list()
-    question_list = [field for field in field_list if re.match(field_pattern, field)]
-    formatted_question_list = [format_question(question) for question in question_list]
+    question_list = [
+        field for field in field_list if re.match(field_pattern, field)]
+
+    formatted_question_list = [format_question(
+        question) for question in question_list]
 
     for index, question in enumerate(question_list):
         formatted_question = formatted_question_list[index]
         answer = row[question]
-        
+
         if formatted_question in ['question_10', 'question_11']:
             if is_valid(answer, int):
                 current_form[formatted_question] = answer
-                
+
         elif is_valid(answer, str):
             current_form[formatted_question] = answer
-            
-            
+
     return current_form
+
 
 def get_residents(row):
     residents = []
-    
+
     # Main resident has to have separate logic because of the different ways the same information is stored
     main_resident = {}
 
     sex = row['sexo']
     if is_valid(sex, str):
         main_resident['sex'] = sex[0].upper()
-    
+
     age = row['idade']
     if is_valid(age, int):
         main_resident['age'] = age
@@ -128,56 +136,70 @@ def get_residents(row):
     education = row['escolaridade']
     if is_valid(education, str):
         main_resident['education'] = education.upper()
-        
+
     residents.append(main_resident)
 
     number_of_residents = row['#pessoas']
     if not is_valid(number_of_residents, int):
         return residents
-    
+
     for resident_index in range(number_of_residents):
         current_resident = {}
-                
-        sex = row['outros mor 1'].split('/')[resident_index]
-        if sex:
-            current_resident['sex'] = sex[0].upper()
+        try:
+            sex = row['outros mor 1'].split('/')[resident_index]
+            if sex:
+                current_resident['sex'] = sex[0].upper()
 
-        age = row['outros mor 2'].split('/')[resident_index]
-        if age and age.isdigit():
-            current_resident['age'] = int(age)
+            age = row['outros mor 2'].split('/')[resident_index]
+            if age and age.isdigit():
+                current_resident['age'] = int(age)
 
-        current_resident['mobility_restriction'] = find_marked_option(row, resident_index, range(3, 9))
-        current_resident['sensory_impairment'] = find_marked_option(row, resident_index, range(9, 12))
-        current_resident['mental_disorder'] = find_marked_option(row, resident_index, range(12, 15))
-        current_resident['health_injury'] = find_marked_option(row, resident_index, range(15, 18))
-        current_resident['education'] = find_marked_option(row, resident_index, range(18, 23))
+            current_resident['mobility_restriction'] = find_marked_option(
+                row, resident_index, range(3, 9))
+            current_resident['sensory_impairment'] = find_marked_option(
+                row, resident_index, range(9, 12))
+            current_resident['mental_disorder'] = find_marked_option(
+                row, resident_index, range(12, 15))
+            current_resident['health_injury'] = find_marked_option(
+                row, resident_index, range(15, 18))
+            current_resident['education'] = find_marked_option(
+                row, resident_index, range(18, 23))
 
-        residents.append(current_resident)
+            residents.append(current_resident)
+        except:
+            pass
 
     return residents
 
 # Finds where the X is marked and gets the answer that it points to
+
+
 def find_marked_option(row, resident_index, option_range):
     for option_number in option_range:
-        current_option = row[f'outros mor {option_number}']
+        try:
+            current_option = row[f'outros mor {option_number}']
+            result = current_option.split("/")[resident_index]
 
-        result = current_option.split("/")[resident_index]
-
-        if result.lower() == 'x':
-            return RESIDENT_QUESTION_RESULTS[option_number]
+            if result.lower() == 'x':
+                return RESIDENT_QUESTION_RESULTS[option_number]
+        except:
+            pass
 
 # Defines whats a valid value for the model object
+
+
 def is_valid(value, instance=None):
     if pandas.isnull(value):
         return False
-    
+
     if instance and not type(value) is instance:
         return False
-    
+
     if type(value) is str and value.startswith('#'):
         return False
-        
+
     return True
+
 
 def format_question(question):
     substitutions = {
