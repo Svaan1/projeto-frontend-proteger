7a3528cb8e65efdac08c0ee37c396726917f82eb
Gustavo B. <gustavobrunodemelo@gmail.com>
2024-07-14 10:57:50 -0300

    fix: style the graphs to not mess the screen

--- frontend/src/lib/components/DashboardFileSection.svelte
+++ frontend/src/lib/components/DashboardFileSection.svelte
@@ -1,70 +1,71 @@
 <script>
-    export let data;
-    import { toast } from "svelte-sonner";
-    import { Button } from "$lib/components/ui/button" 
-    import {uploadFile} from "$lib/api/files.js";
-    import DashboardFileTable from "./DashboardFileTable.svelte";
+  export let data;
+  import { toast } from "svelte-sonner";
+  import { Button } from "$lib/components/ui/button";
+  import { uploadFile } from "$lib/api/files.js";
+  import DashboardFileTable from "./DashboardFileTable.svelte";
 
-    async function handleUploadFile() {
-        const fileInput = document.getElementById("fileInput");
-        const file = fileInput.files[0];
+  async function handleUploadFile() {
+    const fileInput = document.getElementById("fileInput");
+    const file = fileInput.files[0];
 
-        if (!file) {
-            toast.warning("Por favor, selecione um arquivo para enviar.");
-            return;
-        }
-
-        const formData = new FormData();
-        formData.append("file", file);
-
-        let response;
-        try {
-            response = await uploadFile(file, data.accessToken);
-        } catch (error) {
-            toast.error("Ocorreu um erro na hora de enviar, tente novamente.");
-        }
-
-        if (response.status === 201) {
-            fileInput.value = "";
-            location.reload();
-        }
-        else {
-            toast.error("Ocorreu um erro, por favor tente novamente.");
-        }
+    if (!file) {
+      toast.warning("Por favor, selecione um arquivo para enviar.");
+      return;
     }
+
+    const formData = new FormData();
+    formData.append("file", file);
+
+    let response;
+    try {
+      response = await uploadFile(file, data.accessToken);
+    } catch (error) {
+      toast.error("Ocorreu um erro na hora de enviar, tente novamente.");
+    }
+
+    if (response.status === 201) {
+      fileInput.value = "";
+      location.reload();
+    } else if (response.status == 409) {
+      toast.error("Arquivo com esse nome já existe.");
+    } else {
+      toast.error("Ocorreu um erro, por favor tente novamente.");
+    }
+  }
 </script>
 
 <div class="container">
-    <div id="fileUpload">
-        <input type="file" name="file" id="fileInput">
-        <Button class="uploadButton" on:click={handleUploadFile}>Enviar</Button>
-    </div>
-    <div id="fileTable">
-        <DashboardFileTable data={data} />
-    </div>
+  <div id="fileUpload">
+    <input type="file" name="file" id="fileInput" />
+    <Button class="uploadButton" on:click={handleUploadFile}>Enviar</Button>
+  </div>
+  <div id="fileTable">
+    <DashboardFileTable {data} />
+  </div>
 </div>
 
 <style>
-    #fileUpload {
-        display: flex;
-        justify-content: space-around;
-        align-items: center;
-        padding: 10px;
-        border-bottom: 1px solid #ccc;
-    }
-    
-    #fileTable {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        padding: 10px;
-        width: 75%;
-        height: 75%;
-    }
+  #fileUpload {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    padding: 10px;
+    border-bottom: 1px solid #ccc;
+  }
 
-    .container {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-    }
+  #fileTable {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 10px;
+    width: 75%;
+    height: 75%;
+  }
+
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
 </style>
--- frontend/src/lib/components/DashboardMap.svelte
+++ frontend/src/lib/components/DashboardMap.svelte
@@ -6,23 +6,37 @@
 
   let selectedQuestions = [];
   const baseURL = "http://127.0.0.1:3000/d-solo/"; // eventually change this to the actual URL inside docker
-  $: mapDashboardURL = baseURL + "adhqdflvh15ogd/map-dashboard?orgId=1&panelId=1"
+  $: mapDashboardURL =
+    baseURL + "adhqdflvh15ogd/map-dashboard?orgId=1&panelId=1";
 
-  const questionBaseURL = baseURL + "bab249e9-cc93-4af0-833a-a25fe0ac080d/main-dashboard?orgId=1&from=1559858777443&to=1717711577444&panelId=";
+  const questionBaseURL =
+    baseURL +
+    "bab249e9-cc93-4af0-833a-a25fe0ac080d/main-dashboard?orgId=1&from=1559858777443&to=1717711577444&panelId=";
 
   function handleClick() {
     if (selectedQuestions.length === 0) {
-      mapDashboardURL = baseURL + "adhqdflvh15ogd/map-dashboard?orgId=1&panelId=1";
+      mapDashboardURL =
+        baseURL + "adhqdflvh15ogd/map-dashboard?orgId=1&panelId=1";
       return;
     }
-    const selectedQuestionsArrayString = selectedQuestions.join("&var-questions=");
-    const newDashboardURL = baseURL + "adhqdflvh15ogd/map-dashboard?orgId=1&panelId=1" + "&var-questions=" + selectedQuestionsArrayString;
+    const selectedQuestionsArrayString =
+      selectedQuestions.join("&var-questions=");
+    const newDashboardURL =
+      baseURL +
+      "adhqdflvh15ogd/map-dashboard?orgId=1&panelId=1" +
+      "&var-questions=" +
+      selectedQuestionsArrayString;
 
     mapDashboardURL = newDashboardURL;
   }
+
+  let innerHeight;
+  let innerWidth;
 </script>
 
-<Tabs.Root value="map" class="w-[400px]">
+<svelte:window bind:innerHeight bind:innerWidth />
+
+<Tabs.Root value="map" class="h-full">
   <Tabs.List>
     <Tabs.Trigger value="map">Mapa</Tabs.Trigger>
     <Tabs.Trigger value="questions">Questões</Tabs.Trigger>
@@ -30,40 +44,90 @@
   </Tabs.List>
 
   <Tabs.Content value="map">
-    <div class="flex items-center display-grid gap-4"> 
-      <MultiSelectComboBoxGrafana  bind:values={selectedQuestions}/>
+    <div class="flex items-center display-grid gap-4 pb-5">
+      <MultiSelectComboBoxGrafana bind:values={selectedQuestions} />
       <Button on:click={handleClick}>
-          <Search class="h-5 w-5"/>
+        <Search class="h-5 w-5" />
       </Button>
     </div>
-    
-    <iframe title="map-dashboard" src={mapDashboardURL} width="85%" height="70%" frameborder="0"/>
+
+    <iframe
+      title="map-dashboard"
+      src={mapDashboardURL}
+      width={innerWidth * 0.7}
+      height={innerHeight * 0.7}
+      frameborder="0"
+    />
   </Tabs.Content>
 
   <Tabs.Content value="questions">
     Sensitividade Demográfica
-    <div class="grid grid-cols-2 gap-20">
-      <iframe title="q1" src={questionBaseURL+"2"} width="450" height="200" frameborder="0"/>
-      <iframe title="q2" src={questionBaseURL+"3"} width="450" height="200" frameborder="0"/>
-      <iframe title="q3" src={questionBaseURL+"4"} width="450" height="200" frameborder="0"/>
-      <iframe title="q4" src={questionBaseURL+"5"} width="450" height="200" frameborder="0"/>
-      <iframe title="q5" src={questionBaseURL+"6"} width="450" height="200" frameborder="0"/>
-      <iframe title="q6" src={questionBaseURL+"18"} width="450" height="200" frameborder="0"/>      
+    <div class="grid grid-cols-2 gap-5">
+      <iframe
+        title="q3"
+        src={questionBaseURL + "4"}
+        width="500"
+        height="300"
+        frameborder="0"
+      />
+      <iframe
+        title="q4"
+        src={questionBaseURL + "5"}
+        width="500"
+        height="300"
+        frameborder="0"
+      />
+      <iframe
+        title="q5"
+        src={questionBaseURL + "6"}
+        width="500"
+        height="300"
+        frameborder="0"
+      />
+      <iframe
+        title="q6"
+        src={questionBaseURL + "18"}
+        width="500"
+        height="300"
+        frameborder="0"
+      />
     </div>
   </Tabs.Content>
 
   <Tabs.Content value="resident-info">
     Moradores
-    <div class="grid grid-cols-2 gap-20">
-      <iframe title="sex" src={questionBaseURL+"10"} width="450" height="200" frameborder="0"/>                   <!-- Sexo -->
-      <iframe title="age" src={questionBaseURL+"17"} width="450" height="200" frameborder="0"/>                   <!-- Idade -->
-      <iframe title="sensorial-impairment" src={questionBaseURL+"13"} width="450" height="200" frameborder="0"/>  <!-- Deficiência Sensorial -->
-      <iframe title="mobility-impairment" src={questionBaseURL+"12"} width="450" height="200" frameborder="0"/>   <!-- Restrição de Mobilidade -->
-      <iframe title="health-impairment" src={questionBaseURL+"15"} width="450" height="200" frameborder="0"/>     <!-- Agravo de Saúde -->
-      <iframe title="mental-impairment" src={questionBaseURL+"14"} width="450" height="200" frameborder="0"/>     <!-- Portador de Transtorno / Deficiência Mental -->    
-      <iframe title="education" src={questionBaseURL+"16"} width="450" height="200" frameborder="0"/>             <!-- Educação -->
+    <div class="grid grid-cols-2 gap-5">
+      <iframe
+        title="sex"
+        src={questionBaseURL + "10"}
+        width="500"
+        height="300"
+        frameborder="0"
+      />
+      <!-- Sexo -->
+      <iframe
+        title="age"
+        src={questionBaseURL + "17"}
+        width="500"
+        height="300"
+        frameborder="0"
+      />
+      <!-- Idade -->
+      <iframe
+        title="sensorial-impairment"
+        src={questionBaseURL + "13"}
+        width="500"
+        height="300"
+        frameborder="0"
+      />
+      <!-- Deficiência Sensorial -->
+      <iframe
+        title="mobility-impairment"
+        src={questionBaseURL + "12"}
+        width="500"
+        height="300"
+        frameborder="0"
+      />
     </div>
   </Tabs.Content>
 </Tabs.Root>
-
-
