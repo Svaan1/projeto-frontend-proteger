8beba9c564df04607d278ef0edc9634b1106e5fa
Gustavo B. <gustavobrunodemelo@gmail.com>
2024-07-17 19:25:29 -0300

    fix(attempt): remove httpOnly to token cookies

--- frontend/src/routes/login/+page.server.js
+++ frontend/src/routes/login/+page.server.js
@@ -1,53 +1,52 @@
-import {redirect} from "@sveltejs/kit";
+import { redirect } from '@sveltejs/kit'
 
-import {getUser, login} from "$lib/api/auth.js";
+import { getUser, login } from '$lib/api/auth.js'
 
 /** @type {import('./$types').PageServerLoad} */
-export async function load({ cookies }) {
-    const accessToken = cookies.get("accessToken");
+export async function load ({ cookies }) {
+  const accessToken = cookies.get('accessToken')
 
-    if (accessToken) {
-        let user;
-        try {
-            user = await getUser(accessToken);
-        } catch {
-            cookies.delete("/accessToken", { path: "/" });
-            return
-        }
-
-        if (user.ok) {
-            redirect(302, "/dashboard");
-            return
-        }
-
-        cookies.delete("/accessToken", { path: "/" });
+  if (accessToken) {
+    let user
+    try {
+      user = await getUser(accessToken)
+    } catch {
+      cookies.delete('/accessToken', { path: '/' })
+      return
     }
+
+    if (user.ok) {
+      redirect(302, '/dashboard')
+      return
+    }
+
+    cookies.delete('/accessToken', { path: '/' })
+  }
 }
 
 /** @type {import('./$types').Actions} */
 export const actions = {
-    default: async ({ cookies, request }) => {
-        const formData = await request.formData();
+  default: async ({ cookies, request }) => {
+    const formData = await request.formData()
 
-        let response;
-        try {
-            response = await login(formData);
-        } catch {
-            return { success: false, message: "Serviço Indisponível" }
-        }
-
-        const data = await response.json();
-
-        if (response.ok) {
-            cookies.set("accessToken", data.access_token, {
-                httpOnly: true,
-                sameSite: true,
-                path: "/"
-            })
-            redirect(302, "/dashboard")
-        }
-
-        return { success: false, message: data.detail, }
-
+    let response
+    try {
+      response = await login(formData)
+    } catch {
+      return { success: false, message: 'Serviço Indisponível' }
     }
-};
+
+    const data = await response.json()
+
+    if (response.ok) {
+      cookies.set('accessToken', data.access_token, {
+        httpOnly: false,
+        sameSite: true,
+        path: '/'
+      })
+      redirect(302, '/dashboard')
+    }
+
+    return { success: false, message: data.detail }
+  }
+}
--- frontend/src/routes/register/+page.server.js
+++ frontend/src/routes/register/+page.server.js
@@ -1,50 +1,49 @@
-import {redirect} from "@sveltejs/kit";
+import { redirect } from '@sveltejs/kit'
 
-import {getUser, register} from "$lib/api/auth.js";
+import { getUser, register } from '$lib/api/auth.js'
 
 /** @type {import('./$types').PageServerLoad} */
-export async function load({ cookies }) {
-    const accessToken = cookies.get("accessToken");
+export async function load ({ cookies }) {
+  const accessToken = cookies.get('accessToken')
 
-    if (accessToken) {
-        let user;
-        try {
-            user = await getUser(accessToken);
-        } catch {
-            cookies.delete("/accessToken", { path: "/" });
-            return
-        }
-
-        if (user.ok) {
-            redirect(302, "/dashboard");
-            return
-        }
-
-        cookies.delete("/accessToken", { path: "/" });
+  if (accessToken) {
+    let user
+    try {
+      user = await getUser(accessToken)
+    } catch {
+      cookies.delete('/accessToken', { path: '/' })
+      return
     }
+
+    if (user.ok) {
+      redirect(302, '/dashboard')
+      return
+    }
+
+    cookies.delete('/accessToken', { path: '/' })
+  }
 }
 
 /** @type {import('./$types').Actions} */
 export const actions = {
-    default: async ({ cookies, request }) => {
-        const body = await request.formData();
-        const username = body.get("username");
-        const email = body.get("email");
-        const password = body.get("password");
+  default: async ({ cookies, request }) => {
+    const body = await request.formData()
+    const username = body.get('username')
+    const email = body.get('email')
+    const password = body.get('password')
 
-        const response = await register(username, email, password);
-        const data = await response.json();
+    const response = await register(username, email, password)
+    const data = await response.json()
 
-        if (response.ok) {
-            cookies.set("accessToken", data.access_token, {
-                httpOnly: true,
-                sameSite: true,
-                path: "/"
-            })
-            redirect(302, "/dashboard")
-            return
-        }
-        return { success: false, message: data.detail }
-
+    if (response.ok) {
+      cookies.set('accessToken', data.access_token, {
+        httpOnly: false,
+        sameSite: true,
+        path: '/'
+      })
+      redirect(302, '/dashboard')
+      return
     }
-};
+    return { success: false, message: data.detail }
+  }
+}
